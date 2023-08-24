272814c0e317f1e53c16e985c1df156883f01ada
Umid <t19b0009u@jdu.uz>
2023-08-24 19:59:27 +0500

    5-lesson

---
+++ 5-lesson/app.js
@@ -0,0 +1,33 @@
+// Loop 
+// 1. While
+// 2. Do-while
+// 3. For
+
+// Loop yordamida juft va top sonlarni chiqarish
+
+// While
+// let a = 1;
+// while(a <= 100) {
+//     if (a % 2 === 1){
+//         console.log(a);
+//     }
+//     a++
+// }
+
+
+// Do-while
+// let a = 1;
+// do {
+//     if (a % 2 == 1){
+//         console.log(a);
+//     }
+//     a++
+// }while(a <= 50);
+
+
+// For 
+// for (let a = 1; a <= 100; a++) {
+//     if (a % 2 == 1){
+//         console.log(a);
+//     }
+// }
