66317f5dfe5bd5211d68f2b88c2d208e48ac82ec
Umid <t19b0009u@jdu.uz>
2023-09-05 17:19:51 +0500

    Numbers

---
+++ 9-lesson/index.js
@@ -0,0 +1,66 @@
+// Home work
+//Random generator
+// const randomGenerator = (min, max) => {
+//     return Math.trunc(Math.random() * (max - min + 1)) + min;
+// }
+// console.log(randomGenerator(20, 30));
+
+// sign, sqrt, cbrt, pow, abs, round, floor, ceil, trunc
+
+// Sqrt
+// const mySqrt = (number) => number ** (1 / 2);
+// console.log(mySqrt(25));
+
+// Sign
+// const sign = (number) => {
+//     if(number > 0) {
+//         console.log(1);
+//     } else if(number < 0){
+//         console.log(-1);
+//     } else if(number === 0) {
+//         console.log(0);
+//     } else if(number === -0){
+//         console.log(-0); // Faqat -0 ni o'qimayapti
+//     }
+// }
+// sign(0);
+
+// Abs
+// const trunc = (number) => {
+//     console.log(parseInt(number));
+// }
+// trunc(12.222);
+
+// Pow
+// const pow = (num1, num2) => {
+//     console.log(num1 ** num2);
+// }
+// pow(2, 5);
+
+// Abs
+// const abs = (number) => {
+//     if(number > 0) {
+//         console.log(number);
+//     } else if(number < 0){
+//         console.log(-number);
+//     }else{
+//         console.log(0);
+//     }
+// }
+// abs(3);
+
+// Yaxlitlash
+// function yaxlitlash(son) {
+//   let butunQisminson = parseInt(son);
+//   let onlikQismi = son - butunQisminson;
+//   if (onlikQismi >= 0.5) {
+//     return butunQisminson + 1;
+//   } else {
+//     return butunQisminson;
+//   }
+// }
+
+// let son = 5.2;
+// let yaxlitlanganSon = yaxlitlash(son);
+
+// console.log(yaxlitlanganSon);
